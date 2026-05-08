c0a42b18928adc20367681b7784271a00be26a05
Prasannaniroula <Prasannaniroula5@gmail.com>
2026-05-08 22:21:14 +0545

    env file added for production ready code of backend url and frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import{useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Dashboard() {
+  const [metrics, setMetrics] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+    axios.get('${import.meta.env.VITE_API_URL}/metrics')
+    .then(res=>{
+      setMetrics(res.data)
+      setLoading(false)
+    })
+    .catch(err=>{
+      console.log(err)
+      setLoading(false)
+    })
+  },[])
+
   return (
     <div>Dashboard</div>
   )
