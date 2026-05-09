eb13b3ddc1d3b851c0564cbaf4c2d838c2113605
Prasannaniroula <Prasannaniroula5@gmail.com>
2026-05-09 09:22:16 +0545

    Dashboard ui completed and connection with backend

--- frontend/src/pages/Composer.jsx
+++ frontend/src/pages/Composer.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function Composer() {
+
+  const [form, setForm] = useState({
+    name: '',
+    role: '',
+    goal: '',
+    email: ''
+})
+  const[loading, setLoading] = useState
+
   return (
     <div>Composer</div>
   )
