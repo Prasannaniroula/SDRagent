afbc8f6726d4b67aa97f9cf087a01d804d0425b5
Prasannaniroula <Prasannaniroula5@gmail.com>
2026-05-09 09:24:06 +0545

    Revert "Dashboard ui completed and connection with backend"
    This reverts commit eb13b3ddc1d3b851c0564cbaf4c2d838c2113605.

--- frontend/src/pages/Composer.jsx
+++ frontend/src/pages/Composer.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
 
 function Composer() {
-
-  const [form, setForm] = useState({
-    name: '',
-    role: '',
-    goal: '',
-    email: ''
-})
-  const[loading, setLoading] = useState
-
   return (
     <div>Composer</div>
   )
